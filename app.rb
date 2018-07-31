3af338f6b8ea540f7ed42b8aa3154e497eacd546
amulyamari <amulya@saimail.com>
2018-07-31 20:20:00 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -22,7 +22,20 @@ end
   end
 
 
-  get 
+  get '/:operation/:number/:number2'do
+    @operation = params [operation]
+    @number1 = params [number1].to_i
+    @number2 = params [number2].to_i
+    if @operation = "multiply"
+      (@number1 * @number2).to_s
+    elsif @operation = "add"
+      (@number1 + @number2).to_s
+    elsif @operation = "subtract"
+      (@number2 - @number1).to_s
+    elsif @operation = "divide"
+      (@number1 / @number2).to_s
+    else
+      "cannot prefor"
     
   
 
